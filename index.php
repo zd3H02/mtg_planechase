616daf50604d90ff784f47d46e25ec00d697183f
zd3H02 <zd3h02@gmail.com>
2021-01-30 17:36:42 +0900

    refactoring

--- index.php
+++ index.php
@@ -25,11 +25,11 @@ $is_server_request_post = $_SERVER["REQUEST_METHOD"] === "POST";
 if($is_server_request_post){
     if($_POST["reset"]){
         $pdo->exec("
-        UPDATE deck_cards SET state = '';
-    ");
+            UPDATE deck_cards SET state = '';
+        ");
         $stmt = $pdo->query("
-        SELECT COUNT(*) AS num FROM deck_cards;
-    ");
+            SELECT COUNT(*) AS num FROM deck_cards;
+        ");
         $deck_cards_count_ary = $stmt->fetch();
         $deck_cards_count = $deck_cards_count_ary["num"];
 
@@ -50,8 +50,8 @@ if($is_server_request_post){
                 }
                 else{
                     $stmt = $pdo->prepare("
-                SELECT * FROM deck_cards WHERE id = ?;
-            ");
+                        SELECT * FROM deck_cards WHERE id = ?;
+                    ");
                     $stmt->execute(array($tb_id_candidate));
                     $candidate_card = $stmt->fetch();
                     $is_phenomenon_card = $candidate_card["type"] == "現象";
@@ -71,53 +71,52 @@ if($is_server_request_post){
         }
         foreach($decks as $tb_id){
             $stmt = $pdo->prepare("
-            UPDATE deck_cards SET state = ? WHERE id = ?
-        ");
+                UPDATE deck_cards SET state = ? WHERE id = ?
+            ");
             $stmt->execute(array($is_deckes,$tb_id));
         }
     }
     elseif($_POST["darw"]){
         $stmt = $pdo->prepare("
-        SELECT id FROM deck_cards WHERE state = ?;
-    ");
+            SELECT id FROM deck_cards WHERE state = ?;
+        ");
         $stmt->execute(array($is_deckes));
         $not_top_decks = $stmt->fetchAll();
-        $now_deck_cards_num = count($not_top_decks);
-        $max_not_top_decks_i = $now_deck_cards_num - 1;
+        $when_draw_card_deck_num = count($not_top_decks);
+        $max_when_draw_card_deck_i = $when_draw_card_deck_num - 1;
 
         $stmt = $pdo->prepare("
-        SELECT id FROM deck_cards WHERE state = ?;
-    ");
+            SELECT id FROM deck_cards WHERE state = ?;
+        ");
         $stmt->execute(array($is_deck_top));
         $deck_top_card_tb_id_ary = $stmt->fetch();
         $deck_top_card_tb_id = $deck_top_card_tb_id_ary["id"];
-        if($now_deck_cards_num >= 1){
-            $draw_card_i = mt_rand(0, $max_not_top_decks_i);
+        if($when_draw_card_deck_num >= 1){
+            $draw_card_i = mt_rand(0, $max_when_draw_card_deck_i);
 
             $draw_card_tb_id = $not_top_decks[$draw_card_i]["id"];
 
 
             if(isset($deck_top_card_tb_id)){
                 $stmt = $pdo->prepare("
-            UPDATE deck_cards SET state = '' WHERE id = ?;
-        ");
+                    UPDATE deck_cards SET state = '' WHERE id = ?;
+                ");
                 $stmt->execute(array($deck_top_card_tb_id));
             }
 
             $stmt = $pdo->prepare("
-        UPDATE deck_cards SET state = ? WHERE id = ?;
-    ");
+                UPDATE deck_cards SET state = ? WHERE id = ?;
+            ");
             $stmt->execute(array($is_deck_top,$draw_card_tb_id));
         }
         else {
             if(isset($deck_top_card_tb_id)){
                 $stmt = $pdo->prepare("
-            UPDATE deck_cards SET state = '' WHERE id = ?;
-        ");
+                    UPDATE deck_cards SET state = '' WHERE id = ?;
+                ");
                 $stmt->execute(array($deck_top_card_tb_id));
             }
         }
-        $now_deck_cards_num = $now_deck_cards_num - 1;
     }
     elseif($_POST["dice"]){
         $set_dice_roll =  $dice_rolls[mt_rand(1, 6)];
@@ -131,14 +130,13 @@ if($is_server_request_post){
 }
 
 
-
 $stmt = $pdo->prepare("
             SELECT id FROM deck_cards WHERE state = ?;
         ");
 $stmt->execute(array($is_deckes));
-$not_top_decks = $stmt->fetchAll();
-$now_deck_cards_num = count($not_top_decks);
-$max_not_top_decks_i = $now_deck_cards_num - 1;
+$display_not_top_decks = $stmt->fetchAll();
+$display_now_deck_cards_num = count($display_not_top_decks);
+
 
 $stmt = $pdo->prepare("
             SELECT id FROM deck_cards WHERE state = ?;
@@ -147,13 +145,12 @@ $stmt->execute(array($is_deck_top));
 $display_deck_top_card_tb_id_ary = $stmt->fetch();
 $display_deck_top_card_tb_id = $display_deck_top_card_tb_id_ary["id"];
 
+
 $stmt = $pdo->prepare("
             SELECT * FROM deck_cards WHERE id = ?;
         ");
 $stmt->execute(array($display_deck_top_card_tb_id));
 $display_deck_top_card = $stmt->fetch();
-
-
 
 
 $stmt = $pdo->query("
@@ -190,9 +187,9 @@ $now_dice_roll_timestamp = $now_dice_roll_ary["roll_timestamp"];
 	</form>
     <p>ダイスの目 : <spam  class="dice_roll"><?php h($now_dice_roll); ?></spam></p>
     <p>ダイスタイムスタンプ : <spam class="dice_roll_timestamp"><?php h($now_dice_roll_timestamp); ?></spam></p>
-	<p>次元デッキ残り枚数 : <spam class="now_deck_cards_num"><?php h($now_deck_cards_num) ?></spam></p>
+	<p>次元デッキ残り枚数 : <spam class="now_deck_cards_num"><?php h($display_now_deck_cards_num) ?></spam></p>
 	<div class="card_text">
-		<?php if($now_deck_cards_num <= 0) : ?>
+		<?php if($display_now_deck_cards_num <= 0) : ?>
 			<p class="deck_empty">デッキが空です</p>
     	<?php else : ?>
         	<?php if(isset($display_deck_top_card)) : ?>
